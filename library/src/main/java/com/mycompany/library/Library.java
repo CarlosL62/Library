448fdd4eef1ad87eb3ba45cec91b35f2c357daa9
CarlosL62 <128768356+CarlosL62@users.noreply.github.com>
2024-02-24 00:36:12 -0600

    Add files via upload

---
+++ library/src/main/java/com/mycompany/library/Library.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.library;
+
+/**
+ *
+ * @author hamme
+ */
+public class Library {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+}
